e74b51a79f6b3879289cfa2b7a44dcdc2decd308
Jo-graves <157560746+Jo-graves@users.noreply.github.com>
2025-08-29 17:28:26 -0500

    Added functionality to automatically identify and process any delimiter.

--- src/geotam_processor.py
+++ src/geotam_processor.py
@@ -6,29 +6,26 @@ import os
 def get_header(filepath):
     '''Gets header of filepath (e.g., where column headers are located)'''
     with open(filepath, "r+") as pracfile: 
-            for id, line in enumerate(pracfile):
+            for line_index, line in enumerate(pracfile):
                 if "State Cd" in line:
-                    return id
-            
-            
+                    return line_index
 
-def df_splitter(filepath):
+def get_delimiter(filepath):
+    with open(filepath, "r+") as pracfile: 
+            for line_index, line in enumerate(pracfile):
+                if "Fields Delimited by: " in line:
+                    delimiter = line.split("Fields Delimited by: ")[1].split()[0]
+                    if delimiter == "Tab":
+                        delimiter = "\t"
+                    return delimiter
 
-    file_path = Path(os.path.realpath(__file__)).parent
+def get_clean_reference_info():
+ 
+    script_path = Path(os.path.realpath(__file__)).parent
 
-    params = fr"{file_path}/ref_files/tceq_parameters.txt"
-    units = fr"{file_path}/ref_files/tceq_units.txt"
-    site_info = fr"{file_path}/ref_files/tceq_site_locations.txt"
-
-    TCEQ_Header = get_header(filepath)        #get header from get_header function
-
-    RD =  pd.read_csv(filepath, sep = ',', header = TCEQ_Header, #open csv
-            skiprows = [], skip_blank_lines = False, encoding = 'ascii',
-            encoding_errors = 'replace') 
-    
-    RD.dropna(axis = 'columns', inplace = True ) #drop column if all values are na - there's usually a few
-    RD['Datetime'] = RD['Date'].astype(str) + ' '  + RD['Time'] #combine date and time to form datetime
-    RD["dt_string"] = pd.to_datetime(RD["Datetime"], format='%Y%m%d %H:%M')
+    params = fr"{script_path}/ref_files/tceq_parameters.txt"
+    units = fr"{script_path}/ref_files/tceq_units.txt"
+    site_info = fr"{script_path}/ref_files/tceq_site_locations.txt"
 
     try:
         tceq_keys = pd.read_table(params) #open tceq parameter info file to match parameter codes to parameters
@@ -36,10 +33,9 @@ def df_splitter(filepath):
         tceq_site_info = pd.read_table(site_info)
 
     except:
-        tceq_keys = pd.read_table(f"{file_path}/ref_files/tceq_parameters.txt") #open tceq parameter info file to match parameter codes to parameters
-        tceq_units = pd.read_table(f"{file_path}/ref_files/tceq_units.txt") #open tceq unit info file to match unit codes to units
-        tceq_site_info = pd.read_table(f"{file_path}/ref_files/tceq_site_locations.txt")
-
+        tceq_keys = pd.read_table(f"{script_path}/ref_files/tceq_parameters.txt") #open tceq parameter info file to match parameter codes to parameters
+        tceq_units = pd.read_table(f"{script_path}/ref_files/tceq_units.txt") #open tceq unit info file to match unit codes to units
+        tceq_site_info = pd.read_table(f"{script_path}/ref_files/tceq_site_locations.txt")
 
     tceq_keys.rename(columns = {"Parm Code": "Parameter Cd", "Name":"Parameter Name"}, inplace = True)
     tceq_units.rename(columns = {"Code": "Unit Cd", "Description": "Unit Description", "Abbr": "Unit Abbr", "Type": "Unit Type"}, inplace = True)
@@ -48,55 +44,49 @@ def df_splitter(filepath):
     tceq_site_info["Site ID"] = tceq_site_info['Site ID'].str.split(",") # Some sites have more than one site ID - this takes care of that
     tceq_site_info = tceq_site_info.explode("Site ID")
 
-    #print(tceq_site_info["Site ID"].unique())
     tceq_site_info["Site ID"] = tceq_site_info['Site ID'].astype(int)
 
-    #print(tceq_site_info)
+    return tceq_keys, tceq_units, tceq_site_info
 
+def df_splitter(filepath):
 
-    #print(RD)
-    RD_keys = RD.merge(tceq_keys, on = "Parameter Cd", how = "inner")
-    RD_u = RD_keys.merge(tceq_units, on = "Unit Cd", how = "inner")
-    RD_o = RD_u.merge(tceq_site_info, on = "Site ID", how = "inner")
+    TCEQ_Header = get_header(filepath)        #get header from get_header function
+    TCEQ_delimiter = get_delimiter(filepath)
 
-    return RD_o
-
-    # return my_dict
-
-def convert_ref_files():
-    file_path = Path(os.path.realpath(__file__)).parent
-
-    params = fr"{file_path}/ref_files/tceq_parameters.txt"
-    units = fr"{file_path}/ref_files/tceq_units.txt"
-    site_info = fr"{file_path}/ref_files/tceq_site_locations.txt"
-
-    tceq_keys = pd.read_table(params) 
-    tceq_units = pd.read_table(units)
-    tceq_site_info = pd.read_table(site_info)
-
-    # #print(Path(params).with_suffix(".csv"))
-
-    tceq_keys.to_csv(Path(params).with_suffix(".csv"))
-    tceq_units.to_csv(Path(units).with_suffix(".csv"))
-    tceq_site_info.to_csv(Path(site_info).with_suffix(".csv"))
+    df =  pd.read_csv(filepath, sep = TCEQ_delimiter, header = TCEQ_Header, #open csv
+            skiprows = [], skip_blank_lines = False, encoding = 'ascii',
+            encoding_errors = 'replace') 
     
+    df.dropna(axis = 'columns', inplace = True ) #drop column if all values are na - there's usually a few
+    df['Datetime'] = df['Date'].astype(str) + ' '  + df['Time'] #combine date and time to form datetime
+    df["dt_string"] = pd.to_datetime(df["Datetime"], format='%Y%m%d %H:%M')
+    
+    tceq_keys, tceq_units, tceq_site_info = get_clean_reference_info()
 
-def geotam_to_csv(geotam_txt_file, date_start = None, date_end = None, save = False, saved_file_type = "csv"):
+    df_keys = df.merge(tceq_keys, on = "Parameter Cd", how = "inner")
+    df_u = df_keys.merge(tceq_units, on = "Unit Cd", how = "inner")
+    df_o = df_u.merge(tceq_site_info, on = "Site ID", how = "inner")
+
+    return df_o
+
+
+def geotam_to_structured_data(geotam_txt_file, date_start = None, date_end = None, save = False, saved_file_type = "csv"):
 
 
     # Process data
     df_out = df_splitter(geotam_txt_file)
 
+    # Create new column that merges parameter name with units for pivoting
     df_out["Column_Name"] = "TCEQ " + df_out["Parameter Name"] + " (" + df_out["Unit Abbr"] + ")"
 
-
+    # Pivot rows to column format
     df_clean = df_out[["Value", "Column_Name", "dt_string", "Site Name", "Site ID"]]
 
     df_clean_piv = df_clean.pivot(index = ["dt_string", "Site Name", "Site ID"], values = "Value", columns="Column_Name").reset_index()
     df_clean_piv.set_index("dt_string", inplace = True)
     df_clean_piv.insert(2, "Datetime - CST", df_clean_piv.index.tz_localize("Etc/GMT+6"))
     df_clean_piv.reset_index(inplace = True, drop = True)
-    df_clean_piv.columns.name =None
+    df_clean_piv.columns.name = None
 
   
     # Get only specified dates
@@ -118,13 +108,12 @@ def geotam_to_csv(geotam_txt_file, date_start = None, date_end = None, save = Fa
 
         if saved_file_type == "csv":
                 df_clean_piv.to_csv(Path(geotam_txt_file).with_suffix(".csv"))
-                print(f"Processd file saved to: {Path(geotam_txt_file).with_suffix(".csv")}")
+                print(f"Processed file saved to: {Path(geotam_txt_file).with_suffix(".csv")}")
 
         elif saved_file_type == 'parquet':
             df_clean_piv.to_parquet(Path(geotam_txt_file).with_suffix(".gzip"))
-            print(f"Processd file saved to: {Path(geotam_txt_file).with_suffix(".gzip")}")
+            print(f"Processed file saved to: {Path(geotam_txt_file).with_suffix(".gzip")}")
 
-    # print(df_clean_piv.columns)
     
     return df_clean_piv
 
@@ -155,6 +144,6 @@ if __name__ == "__main__":
     args = p.parse_args()
 
     # By running the script directly from the command line, it is assumed the file intends to be saved so the save flag is always set to true
-    geotam_to_csv(geotam_txt_file = args.file2proc, date_start = args.start_date, date_end = args.end_date, save = True, saved_file_type = args.saved_file_type)
+    geotam_to_structured_data(geotam_txt_file = args.file2proc, date_start = args.start_date, date_end = args.end_date, save = True, saved_file_type = args.saved_file_type)
 
     
