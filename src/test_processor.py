efc166e36e11cc33fd2fda9d43997192ab546b7c
Joel Personal <jdg_personal@Joels-Mac-mini.local>
2025-10-05 13:57:01 -0500

    More cleaning and removing some excess files.

--- src/test_processor.py
+++ src/test_processor.py
@@ -11,25 +11,36 @@ import polars.testing as ptesting
 file_path = Path(os.path.realpath(__file__)).parent
 
 def test_comma():
-    # test_file = f"{file_path}/../tests/2023_kc_autogc_w_ws_wd.txt"
+    '''
+    Test if processor works for comma delimited data.
+    
+    '''
     test_file = f"{file_path}/../tests/2025_kc_autogc_w_ws_wd_comma.txt"
-    df = pt.read_tceq_to_pl_dataframe(test_file, save = False, saved_file_type="csv")
+    df = pt.read_tceq_to_pl_dataframe(test_file, save = False)
     assert not df.is_empty()
     return df
 
 def test_pipe():
+
+    '''Test if processor works for pipe-delimited data'''
     test_file = f"{file_path}/../tests/2025_kc_autogc_w_ws_wd_pipe.txt"
-    df = pt.read_tceq_to_pl_dataframe(test_file, save = False, saved_file_type="csv")
+    df = pt.read_tceq_to_pl_dataframe(test_file, save = False)
     assert not df.is_empty()
     return df
 
 def test_tab():
+    """Test if processor works for tab-delimited data"""
     test_file = f"{file_path}/../tests/2025_kc_autogc_w_ws_wd_tab.txt"
-    df = pt.read_tceq_to_pl_dataframe(test_file, save = False, saved_file_type="csv")
+    df = pt.read_tceq_to_pl_dataframe(test_file, save = False)
     assert not df.is_empty()
     return df
 
 def test_equivalence():
+
+    '''
+    Test if comma, pipe, and tab data are all processed identically
+    
+    '''
     df1 = test_comma()
     df2 = test_pipe()
     df3 = test_tab()
@@ -40,7 +51,13 @@ def test_equivalence():
 
 
 
+
 def proc_tceq_formatted_ethane_2025():
+
+    '''
+    Process data formatted by TCEQ (ethane) for comparing with data processed with TCEQ processor package
+    
+    '''
     f1 = f"{file_path}/../tests/202503_kc_autogc_formatted_tceq_ethane.txt"
     df = pl.read_csv(f1).select(pl.exclude("Rec"))
     df = df.unpivot(index = "Day")
@@ -71,6 +88,10 @@ def proc_tceq_formatted_ethane_2025():
 
 
 def compare_tceq_formatted_and_processed_data_ethane_2025():
+
+    '''Compare the tceq formatted data and data processed from geotam_processor package
+    
+    '''
     f1 = f"{file_path}/../tests/2025_kc_autogc_w_ws_wd_tab.txt"
     df = pt.read_tceq_to_pl_dataframe(f1, save = False, saved_file_type="csv")
     df = df.select(pl.col("Datetime", "TCEQ Ethane (ppbv)"))
